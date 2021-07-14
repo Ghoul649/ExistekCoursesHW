4be414de369519d5da84cceed49b00d1de02c1a7
Рома Г <roma.gulchak@gmail.com>
2021-07-14 14:28:49 +0300

    HW4 - completed second task

---
+++ HW4_Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿using HWCommon.Commands;
+using System;
+
+namespace HW4_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CommandHandler manager = new CommandHandler();
+            manager.UseBaseCommands();
+            manager.Init(new Program());
+            manager.Execute("help");
+            manager.UseBaseConsoleIO();
+        }
+        Circle c = new Circle() { Radius = 1 };
+        [Command("Execute delegate")]
+        public void Execute() 
+        {
+            Console.WriteLine($"Result: {c.GetScalar()}");
+        }
+        [Command("Set the radius of the circle")]
+        public void SetRadius(double newRadius) 
+        {
+            c.Radius = newRadius;
+        }
+        [Command("Set function")]
+        public void SetFunction([Param("Type of function. Allowed values: Radius, Length, Diameter, Area")]FunctionsType func) 
+        {
+            c.TransformFunction = FunctionsTypeExtension.GetFunction(func);
+        }
+    }
+}
