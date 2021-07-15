0d16cd9d46cb8b773b9a47b6ed152da769e9f878
Рома Г <roma.gulchak@gmail.com>
2021-07-15 20:13:24 +0300

    created HW6

---
+++ HW6/Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW6
+{
+    class Point
+    {
+        private static int _nextCharNum = 'A';
+        private static int _maxCharNum = 'Z';
+        public double X { get; }
+        public double Y { get; }
+        public char CharName { get; set; }
+        public Point() 
+        {
+            if (_nextCharNum > _maxCharNum)
+                throw new Exception("All characters had been used");
+            CharName = (char)_nextCharNum++;
+        }
+    }
+}
