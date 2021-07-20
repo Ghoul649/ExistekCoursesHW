f1669ceaaf47f46f4495132648e629762967e37c
Рома Г <roma.gulchak@gmail.com>
2021-07-20 17:33:02 +0300

    updated hw7

--- HW7/Core.cs
+++ HW7/Core.cs
@@ -12,36 +12,83 @@ namespace HW7
     class Core
     {
         public Assembly CurrentAssembly;
-        void checkAssembly() 
+        private IEnumerable<Type> _loadedTypes;
+        void checkAssembly()
         {
             if (CurrentAssembly == null)
                 throw new FileLoadException("Assembly is not loaded");
         }
         [Command("Load assembly")]
-        public void Load([QuotedText] string filepath) 
+        public void Load([Line] string filepath)
         {
             if (!File.Exists(filepath))
                 throw new FileNotFoundException("Specified file does not exist");
             CurrentAssembly = Assembly.LoadFrom(filepath);
+            _loadedTypes = CurrentAssembly.GetTypes().Where(t =>
+                !t.IsAbstract &&
+                !t.IsInterface &&
+                !t.IsGenericType
+            );
         }
 
         [Command("Display all types in loaded assembly")]
-        public void List() 
+        public void List()
         {
             checkAssembly();
-            foreach (var type in CurrentAssembly.GetTypes()) 
+            foreach (var type in _loadedTypes)
             {
                 Console.WriteLine(type);
             }
         }
         [Command("Display filtered types in loaded assembly")]
-        public void List([Line]string filter) 
+        public void List([Line] string filter)
         {
             checkAssembly();
-            foreach (var type in CurrentAssembly.GetTypes().Where(t => t.ToString().Contains(filter,StringComparison.OrdinalIgnoreCase)))
+            string[] lines = filter.Split();
+            foreach (var type in _loadedTypes.Where(
+                t =>
+                {
+                    var str = t.ToString();
+                    foreach (var word in lines)
+                        if (!str.Contains(word, StringComparison.OrdinalIgnoreCase))
+                            return false;
+                    return true;
+                }
+                )
+            )
             {
                 Console.WriteLine(type);
             }
+        }
+
+        [Command("Display all members of specified type")]
+        public void Getmembers(string type) 
+        {
+            Type selectedType = _loadedTypes.First(t => t.FullName == type);
+            foreach (var prop in selectedType.GetProperties())
+                Console.WriteLine(prop);
+            foreach (var method in selectedType.GetMethods())
+                Console.WriteLine(method);
+        }
+
+        [Command("Display code of class")]
+        public void WriteCode(string type) 
+        {
+            Type selectedType = _loadedTypes.First(t => t.FullName == type);
+            Generator g = new Generator();
+
+            StringBuilder sb = new StringBuilder();
+
+            g.WriteClass(sb, selectedType);
+
+            foreach (var prop in selectedType.GetProperties())
+            {
+                g.WriteProp(sb, prop);
+                sb.Append('\n');
+            }
+            StringBuilder ns = new StringBuilder();
+            g.WriteNamespaces(ns);
+            Console.WriteLine(ns.ToString() + sb.ToString());
 
         }
     }
---
+++ HW7/Generator.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace HW7
+{
+    public class Generator
+    {
+        public bool AutoProps { get; set; } = true;
+        public List<string> Namespaces { get; private set; } = new List<string>();
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+        public void WriteType(StringBuilder sb, Type t)
+        {
+            if (!Namespaces.Contains(t.Namespace))
+                Namespaces.Add(t.Namespace);
+            if (t.IsGenericType)
+            {
+                sb.Append(t.Name.Substring(0, t.Name.IndexOf('`')));
+                sb.Append('<');
+                for (int i = 0; i < t.GenericTypeArguments.Length; i++)
+                {
+                    WriteType(sb, t.GenericTypeArguments[i]);
+                    if (i + 1 < t.GenericTypeArguments.Length)
+                        sb.Append(", ");
+                }
+                sb.Append('>');
+            }
+            else
+                sb.Append(t.Name);
+        }
+        public void WriteProp(StringBuilder sb, PropertyInfo prop)
+        {
+            bool isPublic = prop.CanWrite && prop.SetMethod.IsPublic;
+            isPublic = isPublic || prop.CanRead && prop.GetMethod.IsPublic;
+
+            if (isPublic)
+                sb.Append("public ");
+            WriteType(sb, prop.PropertyType);
+
+            sb.Append($" {prop.Name}");
+            if (AutoProps)
+                sb.Append(" { ");
+            else
+                sb.Append("\n{\n\t");
+            if (prop.CanRead)
+            {
+                WritePropMethod(sb, prop.GetMethod);
+                if (!AutoProps)
+                {
+                    if (prop.CanWrite)
+                        sb.Append("\n\t");
+                }
+                else
+                    if (prop.CanWrite)
+                    sb.Append(' ');
+            }
+            if (prop.CanWrite)
+                WritePropMethod(sb, prop.SetMethod);
+            sb.Append(AutoProps ? " " : " \n");
+            sb.Append('}');
+
+        }
+        public void WritePropMethod(StringBuilder sb, MethodInfo method)
+        {
+            if (method.IsPrivate)
+                sb.Append("private ");
+            if (method.ReturnType == typeof(void))
+            {
+                sb.Append("set");
+            }
+            else
+            {
+                sb.Append("get");
+            }
+            if (!AutoProps)
+                sb.Append(" => throw new NotImplementedException();");
+            else
+                sb.Append(';');
+        }
+        public void WriteNamespaces(StringBuilder sb)
+        {
+            foreach (var ns in Namespaces)
+                sb.AppendLine($"using {ns};");
+        }
+        public void WriteMethod(StringBuilder sb, MethodInfo method)
+        {
+            if (method.Attributes.HasFlag(MethodAttributes.Public))
+                sb.Append("public ");
+            if (method.Attributes.HasFlag(MethodAttributes.Static))
+                sb.Append("static ");
+            if (method.Attributes.HasFlag(MethodAttributes.Virtual))
+                sb.Append("virtual ");
+            if (method.ReturnType == typeof(void))
+                sb.Append("void ");
+
+            sb.Append(method.Name);
+        }
+        public void WriteClass(StringBuilder sb, Type t)
+        {
+            var methods = t.GetMethods().Where(t =>
+                t.Attributes.HasFlag(MethodAttributes.Public) &&
+                !t.Attributes.HasFlag(MethodAttributes.SpecialName) &&
+                t.GetBaseDefinition() == t
+            );
+            foreach (var method in methods)
+            {
+                WriteMethod(sb, method);
+            }
+        }
+        
+    }
+}
